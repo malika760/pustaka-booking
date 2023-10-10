ae09c991becfe5279d441422ef8a222461436ed6
malika760 <aldika755@gmail.com>
2023-10-10 15:24:00 +0700

    tugas ketiga

--- application/controllers/matakuliah.php
+++ application/controllers/matakuliah.php
@@ -8,13 +8,31 @@ class Matakuliah extends CI_Controller
     }
 
     public function cetak()
+
+   
     {
         $data = [
             'kode' => $this->input->post('kode'),
             'nama' => $this->input->post('nama'),
-            'sks' => $this->input->post('sks'),
+            'sks' =>  $this->input->post('sks'),
         ];
-
+        $this->form_validation->set_rules('kode', 'Kode Matakuliah', 'required|min_length[3]', [
+    
+            'required' => 'Kode Matakuliah Harus diiisi',
+        
+            'min_lenght' => 'Kode terlalu pendek'
+        ]);
+    
+        $this->form_validation->set_rules('nama', 'Nama Matakuliah', 'required|min_length[3]',[
+            'required' => 'Nama Matakuliah Harus diiisi',
+        
+            'min_lenght' => 'Nama terlalu pendek'
+        ]);
+    
+        if ($this->form_validation->run() != true) {
+            $this->load->view('view-form-matakuliah');
+        }
+ 
         $this->load->view('view-data-matakuliah', $data);
     }
 }
