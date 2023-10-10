a6525252d26d888ec03cff51938df1cea393566f
malika760 <aldika755@gmail.com>
2023-10-10 15:45:01 +0700

    validation

--- application/controllers/matakuliah.php
+++ application/controllers/matakuliah.php
@@ -11,11 +11,6 @@ class Matakuliah extends CI_Controller
 
    
     {
-        $data = [
-            'kode' => $this->input->post('kode'),
-            'nama' => $this->input->post('nama'),
-            'sks' =>  $this->input->post('sks'),
-        ];
         $this->form_validation->set_rules('kode', 'Kode Matakuliah', 'required|min_length[3]', [
     
             'required' => 'Kode Matakuliah Harus diiisi',
@@ -31,8 +26,17 @@ class Matakuliah extends CI_Controller
     
         if ($this->form_validation->run() != true) {
             $this->load->view('view-form-matakuliah');
+        } else {
+            $data = [
+                'kode' => $this->input->post('kode'),
+                'nama' => $this->input->post('nama'),
+                'sks' =>  $this->input->post('sks'),
+            ];
+            $this->load->view('view-data-matakuliah', $data);
         }
  
-        $this->load->view('view-data-matakuliah', $data);
+        
+        
+        
     }
 }
